83fd0c0a9db504dbab37c30c0f515e3a03aeb0d9
pysunita <sunitasahu.nl@gmail.com>
2021-05-13 16:56:40 +0200

    jquery codes added

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-  $("h1").css("color","red");
+// $(document).ready(function() {
+//   $("h1").css("color","red");
+// });
+
+$("h1").addClass("big-title margin-50");
+$("button")
+
+$("button").click(function(){
+  $("h1").css("color","purple");
+});
+
+// $("input").keypress(function(event) {
+//   console.log(event.key);
+// });
+
+$(document).keypress(function(event) {
+  $("h1").text(event.key);
+});
+
+
+$("h1").on("click",function(){
+$("h1").css("color", "green")
 });
